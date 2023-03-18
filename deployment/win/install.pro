eb41a3502d899049c7d0be63955f5cb348199b6d
Sagiadinos <info@saghiadinos.de>
2023-03-18 22:05:43 +0100

    add installer for Windows

---
+++ deployment/win/install.pro
@@ -0,0 +1,22 @@
+TEMPLATE = aux
+
+INSTALLER = garlic_install
+
+INPUT = $$PWD/config/config.xml $$PWD/packages
+garlicinstall.input = INPUT
+garlicinstall.output = $$INSTALLER
+unix:garlicinstall.commands = /home/niko/Anwendungen/Qt/Tools/QtInstallerFramework/4.5/bin/binarycreator -c $$PWD/config/config.xml -p $$PWD/packages ${QMAKE_FILE_OUT}
+win32:garlicinstall.commands = C:\Qt\Tools\QtInstallerFramework\4.5\bin\binarycreator -c $$PWD\config\config.xml -p $$PWD\packages ${QMAKE_FILE_OUT}
+garlicinstall.CONFIG += target_predeps no_link combine
+
+QMAKE_EXTRA_COMPILERS += garlicinstall
+
+DISTFILES += \
+    packages/com.sagiadinos.garlic.creator/data/content_to_install.txt \
+    packages/com.sagiadinos.garlic.creator/meta/agpl-3.0.txt \
+    packages/com.sagiadinos.garlic.creator/meta/install.js \
+    config/controllerScript.js \
+    packages/com.microsoft.vcredist.x64/meta/install.js \
+    packages/com.microsoft.vcredist.x64/data/vc_redist.x64.exe \
+    packages/com.microsoft.vcredist.x64/meta/package.xml \
+    packages/com.sagiadinos.garlic.creator/meta/package.xml
---
+++ deployment/win/packages/com.sagiadinos.garlic.creator/meta/install.js
@@ -0,0 +1,38 @@
+function cancelInstaller(message)
+{
+    installer.setDefaultPageVisible(QInstaller.Introduction, false);
+    installer.setDefaultPageVisible(QInstaller.TargetDirectory, false);
+    installer.setDefaultPageVisible(QInstaller.ComponentSelection, false);
+    installer.setDefaultPageVisible(QInstaller.ReadyForInstallation, false);
+    installer.setDefaultPageVisible(QInstaller.StartMenuSelection, false);
+    installer.setDefaultPageVisible(QInstaller.PerformInstallation, false);
+    installer.setDefaultPageVisible(QInstaller.LicenseCheck, false);
+
+    var abortText = "<font color='red'>" + message +"</font>";
+    installer.setValue("FinishedText", abortText);
+}
+
+function Component()
+{
+    if ( systemInfo.currentCpuArchitecture === "i386")
+    {
+        cancelInstaller("Installation on " + systemInfo.currentCpuArchitecture + " architecture is not supported");
+    }
+}
+
+Component.prototype.createOperations = function()
+{
+    component.createOperations();
+
+    if (systemInfo.productType === "windows")
+	{
+        component.addOperation("CreateShortcut", "@TargetDir@/start_garlic.bat", "@StartMenuDir@/garlic-creator.lnk",
+            "workingDirectory=@TargetDir@",
+			"iconPath=@TargetDir@/bin/garlic-creator.ico"
+        );
+        component.addOperation("CreateShortcut", "@TargetDir@/start_garlic.bat", "@DesktopDir@/garlic-creator.lnk",
+            "workingDirectory=@TargetDir@",
+			"iconPath=@TargetDir@/bin/garlic-creator.ico"
+        );
+    }
+}
