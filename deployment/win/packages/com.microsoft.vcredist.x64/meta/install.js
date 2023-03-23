94732adb40cb590564dd8582311bfb42714ae585
sagiadinos <ns@smil-control.com>
2023-03-23 02:15:49 +0100

    fix check for vcredist

--- deployment/win/packages/com.microsoft.vcredist.x64/meta/install.js
+++ deployment/win/packages/com.microsoft.vcredist.x64/meta/install.js
@@ -4,8 +4,9 @@ Component.prototype.createOperations = function()
 {
     try
     {
-        if (installer.value("os") === "win" && installer.value("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\X644\\Installed) !== 1)
+        if (!installer.containsValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\14.0\\VC\\Runtimes\\X64\\Installed"))
         {
+			//
             component.createOperations();
             component.addElevatedOperation("Execute", "@TargetDir@/vcredist_x64.exe", "/quiet", "/norestart");
             component.addElevatedOperation("Delete", "@TargetDir@/vcredist_x64.exe");
